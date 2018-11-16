1797e8c81397b61fa4f3b43efd8f9421aabf9c98
TDeadcoldT <45097173+TDeadcoldT@users.noreply.github.com>
2018-11-16 12:10:38 +0000

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetBoarding.Startup))]
+namespace PetBoarding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
