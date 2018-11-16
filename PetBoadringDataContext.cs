260a46ded1edea0a3b67a35cdad6b45367daedf6
TDeadcoldT <45097173+TDeadcoldT@users.noreply.github.com>
2018-11-16 12:20:12 +0000

    Models

---
+++ PetBoadringDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PetBoarding.Models
+{
+    public class PetBoardingDataContext : DbContext
+        //This code is the actual database where all data that has been either create the data, edit it within, view it or delete from the database.
+    {
+        public DbSet<PetBoarding> PetBoarding { get; set; }
+
+        static PetBoardingDataContext()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<PetBoardingDataContext>());
+        }
+    }
+}
---
+++ PetBoarding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace PetBoarding.Models
+{
+    public class PetBoarding
+    {
+        [Required]
+        //This code allows to create attributes and behaviours for petboarding website.
+        public long id { get; set; }
+        [Required]
+        //This code allows to generate labels and textboxes for the model.
+        [DisplayName("Animal")]
+        public string Animal { get; set; }
+        [Required]
+        [DisplayName("Species")]
+        public string Species { get; set; }
+        [Required]
+        [DisplayName("Age")]
+        public string Age { get; set; }
+        [Required]
+        [DisplayName("Country")]
+        public string Country { get; set; }
+        [Required]
+        [DisplayName("Address")]
+        public string Address { get; set; }
+        [Required]
+        [DisplayName("Postcode")]
+        public string Postcode { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+        [Required]
+        public Boolean Approved { get; set; }
+        public string Description { get; set; }
+
+    }
+}
